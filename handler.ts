959de2c8321a0102fdb67d4ee9fe18b67246228b
Federico Sierra <fedesierr@gmail.com>
2020-04-29 18:15:42 -0300

    initial commit

---
+++ handler.ts
@@ -0,0 +1,57 @@
+import 'source-map-support/register';
+import * as AWS from 'aws-sdk';
+
+export interface Target {
+  services: Service[]
+}
+
+export interface Service {
+    cluster: string,
+    service: string
+}
+
+function updateService(ecs: AWS.ECS, params: AWS.ECS.UpdateServiceRequest): Promise<string> {
+    return new Promise((resolve) => {
+        ecs.updateService(params, function (err, data) {
+            if (err) {
+                console.log(err, err.stack); // An error occurred
+                resolve(`${params.service} not updated`);
+            } else {
+                console.log(data); // Successful response
+                resolve(`${params.service} updated => Desired count: ${params.desiredCount}`)
+            }
+        });
+    })
+}
+
+export const stop = async (event: Target): Promise<string[]> => {
+    const ecs = new AWS.ECS();
+    const promises = new Array<Promise<any>>();
+    console.log(event);
+    event.services.forEach(target => {
+            const params: AWS.ECS.UpdateServiceRequest = {
+                cluster: target.cluster,
+                service: target.service,
+                desiredCount: 0
+            };
+            promises.push(updateService(ecs, params))
+        }
+    )
+    return Promise.all(promises)
+};
+
+export const start = async (event: Target): Promise<string[]> => {
+  const ecs = new AWS.ECS();
+  const promises = new Array<Promise<any>>();
+
+  event.services.forEach(target => {
+        const params: AWS.ECS.UpdateServiceRequest = {
+          cluster: target.cluster,
+          service: target.service,
+          desiredCount: 1
+        };
+        promises.push(updateService(ecs, params))
+      }
+  )
+  return Promise.all(promises)
+};
